528f57532f3c8672f3a62978032b005e4746767e
Henri Stahl <henrystahl@rocketmail.com>
2018-08-16 18:09:14 +0200

    index hp error

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/create'
+  get 'cocktails/edit'
+  get 'cocktails/update'
+  get 'cocktails/delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :cocktails
+root to: "cocktails#index"
 end
